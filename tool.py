9ef6fd34446356da593747765a0a72b61b6fb1d3
kapil <duckatgit@gmail.com>
2025-10-16 15:51:36 +0530

    Updated tool code via Admin Panel

--- tool.py
+++ tool.py
@@ -8,23 +8,12 @@ DO NOT add lambda_handler code here - it will be added automatically.
 """
 
 def main(event_body):
-    """
-    Main function for TestTool3Local
-    This function will be called by the Lambda handler.
-    
-    Parameters:
-    event_body: dict - The request body passed to the Lambda function
-    
-    Returns:
-    dict - JSON-serializable response
-    """
-    # Your tool logic here
-    # Access parameters from event_body
-    # Example: user_input = event_body.get('input_data', '')
-    
+    a=10
+    b=20
+       
     return {
         "success": True,
-        "message": "Hello from TestTool3Local!",
+        "message": a+b,
         "data": event_body
     }
 
